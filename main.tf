06532d52a389a604616e995915a424cd18642c4d
ravisharma4167 <ravisharma4167@gmail.com>
2024-12-29 23:12:34 +0530

    gitrg2 git lock test

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rg1" {
   name     = "gitrg1"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "rg2" {
+  name     = "gitrg2"
+  location = "West Europe"
+}
